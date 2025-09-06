0c0780700f89e012e7d0dd579995a4032a5e4f17
yogesh9275 <yogesh.rudra@avcitsolutions.com>
2025-09-06 12:25:51 +0530

    update sidebar

--- Habit-Tracker/resources/views/components/layouts/app/sidebar.blade.php
+++ Habit-Tracker/resources/views/components/layouts/app/sidebar.blade.php
@@ -2,6 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
+     <script>
+        document.addEventListener("alpine:init", () => {
+            // If no user preference stored, default to light
+            if (!localStorage.getItem('flux.appearance')) {
+                localStorage.setItem('flux.appearance', 'light');
+            }
+        });
+    </script>
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
